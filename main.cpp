1e25782ef6d1641cae3aa4e93d89f04dc911f9d7
bilal <bilalitani1@gmail.com>
2015-06-05 10:37:03 -0400

    Organize code in main file

--- main.cpp
+++ main.cpp
@@ -6,30 +6,16 @@
 #include <QtSql>
 #include <QtDebug>
 #include <QtSql/QSqlDriver>
-#include <QFontDatabase>
 
-int main(int argc, char *argv[])
+/* Sets up the login screen and displays it. */
+void DisplayLogin(QtQuick2ApplicationViewer* viewer, QGuiApplication* app)
 {
-    QGuiApplication app(argc, argv);
+    viewer->setMainQmlFile(QStringLiteral("qml/machinesLobby/login.qml"));
+    viewer->showExpanded();
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setHostName("localhost");
-    db.setPort(7777);
-    db.setDatabaseName("machines_users");
-    db.setUserName("root");
-    db.setPassword("");
-    bool ok = db.open();
+    viewer->setColor("transparent");
 
-    qDebug() << ok;
-
-    QtQuick2ApplicationViewer viewer;
-
-    viewer.setMainQmlFile(QStringLiteral("qml/machinesLobby/login.qml"));
-    viewer.showExpanded();
-
-    viewer.setColor("transparent");
-
-    QScreen* screen = app.primaryScreen();
+    QScreen* screen = app->primaryScreen();
     const QRect mainScreenSize = screen->availableGeometry();
 
     //int width = mainScreenSize.width() * .7;
@@ -37,15 +23,41 @@ int main(int argc, char *argv[])
 
     //viewer.resize(width, height);
 
-    viewer.setFlags(Qt::FramelessWindowHint);
+    viewer->setFlags(Qt::FramelessWindowHint);
 
-    viewer.setParent(0);
+    viewer->setParent(0);
 
     // Access context to move window
-    QQmlContext* context = viewer.rootContext();
-    context->setContextProperty("viewerWindow", &viewer);
+    QQmlContext* context = viewer->rootContext();
+    context->setContextProperty("viewerWindow", viewer);
+}
 
 
+bool OpenDatabase(QString hostName, int port, QString databaseName, QString userName, QString password)
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setHostName(hostName);
+    db.setPort(port);
+    db.setDatabaseName(databaseName);
+    db.setUserName(userName);
+    db.setPassword(password);
+    bool ok = db.open();
+
+    return ok;
+}
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    bool ok = OpenDatabase("localhost", 7777, "machines_users", "root", "");
+
+    qDebug() << ok;
+
+    QtQuick2ApplicationViewer viewer;
+
+    DisplayLogin(&viewer, &app);
 
     return app.exec();
 }
+
